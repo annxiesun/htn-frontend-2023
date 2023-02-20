14cbf50cd887dbe4d07cd9281b281f5dfb1da3ed
Annie Sun <anniesun@Annies-MacBook-Pro.local>
2023-02-19 19:45:16 -0500

    set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { ApplicationContextProvider } from './context';
+import { Dashboard } from './dashboard';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ApplicationContextProvider>
+        <RouterProvider router={router} />
+      </ApplicationContextProvider>
     </div>
   );
 }
